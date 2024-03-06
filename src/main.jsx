095ebe2d67207dcff78e664106ac5a59b88b6324
AJ <99290636+miserysauce@users.noreply.github.com>
2024-03-05 19:44:31 -0600

    Revert "trying something else"
    This reverts commit 03c97ca6b732e9f11a262f68747aec28cf82a9de.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <HashRouter base="/"> 
-  <App />
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
